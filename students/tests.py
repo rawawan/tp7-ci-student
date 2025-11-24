64abd35973e5e088704b0706dcfe15f52de85427
rawawan <raouane.bendahmane@gmail.com>
2025-11-24 18:24:09 +0100

    writing a test test

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class SimpleTest(TestCase):
+
+    def test_user_creation(self):
+        user = User.objects.create_user(username="testuser", password="pass1234")
+
+        # Vérifier que l'utilisateur existe bien
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(user.username, "testuser")
